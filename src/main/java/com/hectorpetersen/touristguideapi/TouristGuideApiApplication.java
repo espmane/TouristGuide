8bfcc723408bb309eaaf3d963fcb7f54eed1ced1
HectorPetersen <hector.guldager.petersen@gmail.com>
2026-01-28 09:37:58 +0100

    packages

--- src/main/java/com/hectorpetersen/touristguideapi/TouristGuideApiApplication.java
+++ src/main/java/com/hectorpetersen/touristguideapi/TouristGuideApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TouristGuideApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TouristGuideApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TouristGuideApiApplication.class, args);
+    }
 
 }
