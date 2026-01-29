b1ca66ceb5f2906bc5bd457ac47d6ca6f44c7b41
linuxcompas <kalikeylogger00@gmail.com>
2026-01-29 11:11:37 +0100

    TouristRepository.getmappers

--- src/main/java/com/hectorpetersen/touristguideapi/controller/TouristController.java
+++ src/main/java/com/hectorpetersen/touristguideapi/controller/TouristController.java
@@ -1,10 +1,20 @@
 package com.hectorpetersen.touristguideapi.controller;
 
+import com.hectorpetersen.touristguideapi.model.TouristAttraction;
+import com.hectorpetersen.touristguideapi.service.TouristService;
+import com.sun.net.httpserver.HttpsServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 
 
 @Controller
@@ -12,6 +22,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class TouristController {
 
-    @GetMapping
+    private final TouristService service;
 
+    public TouristController(TouristService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TouristAttraction>> GetAll() {
+        List<TouristAttraction> attractions = service.getAllAttractions();
+        return new ResponseEntity<>(attractions, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<TouristAttraction> getByName(@PathVariable String name) {
+        TouristAttraction found = service.findAttractionsByName(name);
+        if (found == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(found, HttpStatus.OK);
+    }
 }
